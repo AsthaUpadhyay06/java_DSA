65cc0852e85cba96c493efccd1c1564f2e11453c
Astha Upadhyay <asthaupadhyay046@gmail.com>
2026-05-21 23:50:14 +0530

    added

--- may.java
+++ may.java
@@ -701,21 +701,72 @@ Scanner sc=new Scanner(System.in);
 
 
 
-int n=5;
-        int mid = n / 2 + 1;
+// int n=5;
+//         int mid = n / 2 + 1;
 
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= n; j++) {
+//         for(int i = 1; i <= n; i++) {
+//             for(int j = 1; j <= n; j++) {
 
-                if(i == mid || j == mid) {
-                    System.out.print("*");
-                }
-                else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println( );
-        }
+//                 if(i == mid || j == mid) {
+//                     System.out.print("*");
+//                 }
+//                 else {
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println( );
+//         }
+
+
+
+
+//Floyd's triangle
+
+// int n=4;
+// int a=1;
+// for(int i=1;i<=n;i++){
+//     for(int j=1;j<=i;j++){
+//         System.out.print(a);
+//         a++;
+//     }
+   
+//     System.out.println();
+// }
+
+
+// Binary's triangle
+
+// int n=5;
+// int a=1;
+// for(int i=1;i<=n;i++){
+//     for(int j=1;j<=i;j++){
+//         if((i+j)%2==0){
+//             System.out.print("1");
+//         }else{
+// System.out.print("0");
+//         }
+        
+       
+//     }
+   
+//     System.out.println();
+// }
+
+
+int n=9;
+
+for(int i=1;i<=n;i=i+2){
+    for(int j=1;j<=i;j=j+2){
+        System.out.print(j);
+        
+    }
+   
+    System.out.println(  );
+}
+
+
+
+
     }
 }
 
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,7 +1,8 @@
-int n=4;
-// for(int i=1; i<=n; i++){
-//     for(int j=1; j<=n+1-i; j++){
-//         System.out.print((char)(96+j) + " ");
-//     }
-//     System.out.println();
-// }
+for(int i=1;i<=n;i+2){
+    for(int j=1;j<=i;j++){
+        System.out.print(i);
+        
+    }
+   
+    System.out.println();
+}
