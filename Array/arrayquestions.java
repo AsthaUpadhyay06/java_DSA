4e5634da287357ac1273f22f0076d12ed01b1f6b
Astha Upadhyay <asthaupadhyay046@gmail.com>
2026-05-28 23:55:02 +0530

    f

--- Array/arrayquestions.java
+++ Array/arrayquestions.java
@@ -41,15 +41,22 @@ public class arrayquestions {
         
 
         
-        int [] arr={-4,5,3,89 ,654,-34,2};
-        int max=arr[0];
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]>max){
-                max=arr[i];
+        // int [] arr={-4,5,3,89 ,654,-34,2};
+        // int max=arr[0];
+        // for(int i=0;i<arr.length;i++){
+        //     if(arr[i]>max){
+        //         max=arr[i];
                 
-            }
-        }
+        //     }
+        // }
         
-        System.out.println(max);
+        // System.out.println(max);
+
+
+       
+
+
+
+
     }
 }
